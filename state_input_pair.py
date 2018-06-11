cf6bb5ab600307d3278b2a6f808398b2730a527c
imosafi <itai.mosafi@live.biu.ac.il>
2018-06-12 00:52:56 +0300

    Fixing null state bug

--- state_input_pair.py
+++ state_input_pair.py
@@ -5,8 +5,8 @@ class StateInputPair(object):
 
     def __init__(self, state, input):
         self.input = input
+        self.state = None
         if state != None:
             self.state = Calculator(state['stack'], state['display'],
                                     state['is_operator_in_stack'], state['is_stack_head_a_result'], state['is_invalid_input'])
-        else:
-            self.state = Calculator()
+
